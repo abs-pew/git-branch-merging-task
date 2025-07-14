0049d113cf4e95e575b0897e5ecbc9d0a59a216d
shakibzk <Schakibzk@gmail.com>
2025-07-14 11:20:55 +0200

    Shakibs Update

--- src/FeatureB.java
+++ src/FeatureB.java
@@ -1,11 +1,14 @@
 public class FeatureB {
 
     public static void main(String[] args) {
+        System.out.println("Das ist Feature B von: Shakib!");
+
         System.out.println("Das ist Feature B von: Shakib ");
 
         int a = 1;
         int b = 2;
         System.out.println(addInts(a, b));
+        System.out.println("Hallo");
     }
     public static int addInts(int a, int b) {
         return a + b;
