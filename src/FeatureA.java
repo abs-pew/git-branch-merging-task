122151742dbe21c0f0322691585b3b0e2eea578a
Regina Michel <regina.michel@stud.tu-darmstadt.de>
2025-07-14 11:12:24 +0200

    changed feature a, added new method to feature b.

--- src/FeatureA.java
+++ src/FeatureA.java
@@ -4,5 +4,7 @@ public class FeatureA {
         System.out.println("Hallo, das ist der Code von Florian");
         System.out.println("Hallo, das ist der Code von <Name>");
         System.out.println("von Abdul");
+
+        System.out.println("Hallo hier ist der Versuch, einen neuen Konflikt zu erzeugen. :)");
     }
 }
